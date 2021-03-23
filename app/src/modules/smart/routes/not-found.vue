9b5d83664983a5af7fbee64ab7b6cab3b9e5a387
ernst86913 <roman.ernst@smartsolutions.cc>
2021-03-24 00:11:33 +0100

    ok

---
+++ app/src/modules/smart/routes/not-found.vue
@@ -0,0 +1,31 @@
+<template>
+	<smart-view :title="$t('page_not_found')">
+		<template #navigation>
+			<files-navigation />
+		</template>
+
+		<div class="not-found">
+			<v-info :title="$t('page_not_found')" icon="not_interested">
+				{{ $t('page_not_found_body') }}
+			</v-info>
+		</div>
+	</smart-view>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+import FilesNavigation from '../components/navigation.vue';
+
+export default defineComponent({
+	components: { FilesNavigation },
+});
+</script>
+
+<style lang="scss" scoped>
+.not-found {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	padding: 20vh 0;
+}
+</style>
---
+++ app/src/views/smart/index.ts
@@ -0,0 +1,4 @@
+import SmartView from './smart-view.vue';
+
+export { SmartView };
+export default SmartView;
